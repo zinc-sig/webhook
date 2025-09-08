00c076df01c82cf602baa0e7e546b6bf30ccea0e
hoifanrd <twbrianchung@gmail.com>
2025-09-08 03:17:25 +0000

    Created function ProvideMockCacheService that provides a cache.Service using fx

--- pkg/mock/mock.go
+++ pkg/mock/mock.go
@@ -4,12 +4,18 @@ import (
 	"context"
 	"crypto/rand"
 	"crypto/rsa"
+	"fmt"
 	"testing"
 	"time"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/machinebox/graphql"
+	container "github.com/narwhl/mockestra/redis"
 	"github.com/stretchr/testify/mock"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/zinc-sig/webhook/pkg/cache"
+	"go.uber.org/fx"
 )
 
 type MockGraphQLClient struct {
@@ -20,6 +26,40 @@ type MockGraphQLClient struct {
 func (m *MockGraphQLClient) Run(ctx context.Context, req *graphql.Request, resp interface{}) error {
 	args := m.Called(ctx, req, resp)
 	return args.Error(0)
+}
+
+func ProvideMockCacheService(t *testing.T) fx.Option {
+	return fx.Options(
+		fx.NopLogger,
+		fx.Supply(
+			fx.Annotate(
+				"8-alpine",
+				fx.ResultTags(`name:"redis_version"`),
+			),
+		),
+		fx.Supply(fx.Annotate(
+			fmt.Sprintf("redis-test-%x", time.Now().Unix()),
+			fx.ResultTags(`name:"prefix"`),
+		)),
+		container.Module(),
+		fx.Provide(func(params struct {
+			fx.In
+			Container testcontainers.Container `name:"redis"`
+		}) *redis.Options {
+			endpoint, err := params.Container.PortEndpoint(t.Context(), container.Port, "")
+			if err != nil {
+				t.Errorf("failed to get endpoint: %v", err)
+			}
+			return &redis.Options{
+				Addr:     endpoint,
+				Password: "", // no password set
+				DB:       0,  // use default DB
+			}
+		}),
+		fx.Provide(
+			cache.NewService,
+		),
+	)
 }
 
 func generateTestToken(t *testing.T) (string, *rsa.PrivateKey) {
