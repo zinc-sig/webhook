b073fd491c05f139848a133ca6057e682e5474ca
hoifanrd <twbrianchung@gmail.com>
2025-09-08 04:18:13 +0000

    Added test on race condition for pkg/cache

--- pkg/cache/cache_test.go
+++ pkg/cache/cache_test.go
@@ -1,6 +1,7 @@
 package cache_test
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -37,7 +38,7 @@ func TestLlen(t *testing.T) {
 }
 
 func TestLoadBalancePublish(t *testing.T) {
-	t.Run("success", func(t *testing.T) {
+	t.Run("logic success", func(t *testing.T) {
 		app := fxtest.New(
 			t,
 			mock.ProvideMockCacheService(t),
@@ -66,6 +67,47 @@ func TestLoadBalancePublish(t *testing.T) {
 					assert.Equal(t, int64(1), length1)
 					assert.Equal(t, int64(1), length2)
 					assert.Equal(t, int64(1), length3)
+				},
+			),
+		)
+
+		app.RequireStart()
+		t.Cleanup(app.RequireStop)
+	})
+
+	t.Run("race condition success", func(t *testing.T) {
+		app := fxtest.New(
+			t,
+			mock.ProvideMockCacheService(t),
+			fx.Invoke(
+				func(cache cache.Service) {
+					var wg sync.WaitGroup
+
+					channels := []string{"channel1", "channel2", "channel3"}
+					for range 99 {
+						wg.Add(1)
+						go func() {
+							defer wg.Done()
+							err := cache.LoadBalancePublish(t.Context(), channels, []byte("hello"))
+							assert.NoError(t, err, "failed to load balance publish")
+						}()
+					}
+					wg.Wait()
+
+					// Check that messages are distributed across channels
+					length1, err := cache.Llen(t.Context(), "channel1")
+					assert.NoError(t, err, "failed to get list length for channel1")
+
+					length2, err := cache.Llen(t.Context(), "channel2")
+					assert.NoError(t, err, "failed to get list length for channel2")
+
+					length3, err := cache.Llen(t.Context(), "channel3")
+					assert.NoError(t, err, "failed to get list length for channel3")
+
+					assert.Equal(t, int64(33), length1)
+					assert.Equal(t, int64(33), length2)
+					assert.Equal(t, int64(33), length3)
+
 				},
 			),
 		)
